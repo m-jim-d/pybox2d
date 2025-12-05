84c26e219a563c59795cf7f909d14346cb3c50a9
m-jim-d <jim.miller.tc@gmail.com>
2025-12-04 22:15:24 -0600

    build.yml updated

--- publish.bat
+++ publish.bat
@@ -3,8 +3,9 @@
 :: script for publishing pybox2d files to Github repository (...)
 
 git add .
-:: git commit -am "readme updated"
-:: git push origin master
 
-git commit --amend -m "Fix contact normal rendering: correct Y-flip, wrap position in b2Vec2, iterate only valid manifold points"
-git push origin master --force
+git commit -am "build.yml updated"
+git push origin master
+
+:: git commit --amend -m "Fix contact normal rendering: correct Y-flip, wrap position in b2Vec2, iterate only valid manifold points"
+:: git push origin master --force
